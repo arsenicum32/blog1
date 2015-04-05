02c4e706ca7817f65e18ba821397c082bf8ffc7b
arsenicum32 <zombiehot@yandex.ru>
2015-04-05 21:16:13 +0300

    stupidmen

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ $('#events').hide();
 $('#FAQ').hide();
 
 var changing = 0;
-var HelpLoading = true; var HelpLoading2 = true,var HelpLoading3 = true;
+var HelpLoading = true; var HelpLoading2 = true; var HelpLoading3 = true;
 
 //$('body').scrollspy({ target: '.jumbotron' });
 
